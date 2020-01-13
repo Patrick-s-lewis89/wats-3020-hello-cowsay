a938d9ff6530907f87fab8f44cf53b86067866ab
patrick-s-lewis89 <patrick.s.lewis7@gmail.com>
2020-01-13 15:54:03 +1300

    fulfill requirements

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-
-//TODO require cowsay
-
-//TODO require getargs
+const cowsay = require("cowsay");
+const getargs = require("./modules/get-args")
 
 // get first string from command line
 let textArg = getargs.getStringArg()
@@ -9,9 +7,9 @@ let textArg = getargs.getStringArg()
 // if use entered a string cowsay it with
 // default eyes and tail
 if (textArg != null) {
-  console.log(cowsay.say({
-    text: textArg,
-    e: "oO",
-    T: "U "
-  }));
+    console.log(cowsay.say({
+        text: textArg,
+        e: "oO",
+        T: "U "
+    }));
 }
